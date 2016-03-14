8cc404c7b9da3852ec47825fd0a916447c1c4e68
Jean-Baptiste Martin <git@martinjb.com>
2016-03-14 05:10:49 +0100

    update SITE URL

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 
 define('DIRECTORY_URL', 'https://webapplis.utc.fr/Trombi_ws/mytrombi/result?');
 define('PHOTO_URL', 'https://demeter.utc.fr/portal/pls/portal30/portal30.get_photo_utilisateur?username=');
-define('SITE_URL', 'http://localhost:8080/sr03_projet_trombi/');
+define('SITE_URL', 'http://trombi-martinjb.rhcloud.com/');
 define('SEARCH_URL', SITE_URL . 'search.php?name=');
