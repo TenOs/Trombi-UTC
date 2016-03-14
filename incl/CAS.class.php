5446357a1e5f5acbd7c5479425402618df511632
Jean-Baptiste Martin <git@martinjb.com>
2016-03-14 14:23:21 +0100

    bug fixes ?

--- incl/CAS.class.php
+++ incl/CAS.class.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'config.php';
 require_once 'incl/xmlToArrayParser.class.php';
 class CAS
 {
@@ -6,7 +7,7 @@ class CAS
 	public static function authenticate()
 	{
 		if (!isset($_GET['ticket']) || empty($_GET['ticket'])) return -1;
-		$data = file_get_contents(self::URL.'serviceValidate?service=http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'&ticket='.$_GET['ticket']);
+		$data = file_get_contents(self::URL.'serviceValidate?service='.SITE_URL.'&ticket='.$_GET['ticket']);
 		if (empty($data)) return -1;
 		$parsed = new xmlToArrayParser($data);
 		if (!isset($parsed->array['cas:serviceResponse']['cas:authenticationSuccess']['cas:user'])) return -1;
@@ -14,11 +15,11 @@ class CAS
 	}
 	public static function login()
 	{
-		header('Location: '.self::URL.'login?service=http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+		header('Location: '.self::URL.'login?service='.SITE_URL);
 	} 
 	public static function logout()
 	{
-		header('Location: '.self::URL.'logout?service=http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']));//ou SCRIPT_NAME?
-		// On n'utilise pas REQUEST_URI sinon cela déconnecterait à l'infini.
+		header('Location: '.self::URL.'logout?service='.SITE_URL);//ou SCRIPT_NAME?
+		// On n'utilise pas REQUEST_URI sinon cela dï¿½connecterait ï¿½ l'infini.
 	}
 }
