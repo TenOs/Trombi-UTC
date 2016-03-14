edc5864660718860cdf037c679939dc247be3cc6
Jean-Baptiste Martin <git@martinjb.com>
2016-03-14 05:42:00 +0100

    update site url again

--- config.php
+++ config.php
@@ -2,5 +2,5 @@
 
 define('DIRECTORY_URL', 'https://webapplis.utc.fr/Trombi_ws/mytrombi/result?');
 define('PHOTO_URL', 'https://demeter.utc.fr/portal/pls/portal30/portal30.get_photo_utilisateur?username=');
-define('SITE_URL', 'http://trombi-martinjb.rhcloud.com/');
+define('SITE_URL', 'http://www.trombi.martinjb.com/');
 define('SEARCH_URL', SITE_URL . 'search.php?name=');
